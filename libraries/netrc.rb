8b063bea6f1717486c49696bb41dbcb741d86c02
John Bellone <jbellone@bellone.us>
2015-11-10 18:52:13 -0500

    Fixes mode problem with netrc write.

--- libraries/netrc.rb
+++ libraries/netrc.rb
@@ -16,25 +16,21 @@ module NetrcCookbook
       attribute(:path, kind_of: String, name_attribute: true)
       attribute(:owner, kind_of: String, default: 'root')
       attribute(:group, kind_of: String, default: 'root')
-      attribute(:mode, kind_of: String, default: '0640')
 
       attribute(:machines, kind_of: Array, default: [])
 
       action(:create) do
-        notifying_block do
-          netrc = ::Netrc.read(new_resource.path)
+        netrc = ::Netrc.read(new_resource.path)
 
-          new_resource.machines.each do |m|
-            netrc[m['host']] = m['login'], m['password']
-          end
-
-          netrc.save
-          FileUtils.chown(new_resource.owner,
-                          new_resource.group,
-                          new_resource.path)
-          FileUtils.chmod(new_resource.mode.to_i(8),
-                          new_resource.path)
+        new_resource.machines.each do |m|
+          netrc[m['host']] = m['login'], m['password']
         end
+
+        new_resource.updated_by_last_action = true if netrc.save
+        FileUtils.chown(new_resource.owner,
+                        new_resource.group,
+                        new_resource.path)
+        FileUtils.chmod(384, new_resource.path)
       end
 
       action(:delete) do
